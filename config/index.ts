c0b2a6df729d47dd88cceec789f46ec1ebc260b9
lws803 <lws803@gmail.com>
2022-01-31 17:12:52 +0800

    optional default redis db index

--- config/index.ts
+++ config/index.ts
@@ -16,7 +16,7 @@ const schema = Joi.object({
   HOST_URL: Joi.string().required(),
   // Redis config
   REDIS_DB_HOST: Joi.string().required(),
-  REDIS_DB_INDEX: Joi.number().required(),
+  REDIS_DB_INDEX: Joi.number().default(0),
   REDIS_DB_PORT: Joi.number().default(6379),
   // Mail config
   MAIL_HOST: Joi.string().required(),
