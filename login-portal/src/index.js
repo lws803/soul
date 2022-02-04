d7f12c41a7463b7aed5191d15ba1a1aba3baa4e7
lws803 <lws803@gmail.com>
2022-02-05 02:16:23 +0800

    center form

--- login-portal/src/index.js
+++ login-portal/src/index.js
@@ -7,6 +7,7 @@ import {
   FormHelperText,
   FormErrorMessage,
   Button,
+  Center,
 } from '@chakra-ui/react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -31,45 +32,49 @@ export default function App() {
 
   return (
     <ChakraProvider>
-      <Box p={8} maxWidth={500}>
-        <form onSubmit={formik.handleSubmit}>
-          <FormControl isInvalid={formik.errors.email && formik.touched.email}>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input
-              id="email"
-              name="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-            {!formik.errors.email && (
-              <FormHelperText>We'll never share your email.</FormHelperText>
-            )}
-            {formik.errors.email && (
-              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-            )}
-          </FormControl>
+      <Center>
+        <Box p={8} width={500}>
+          <form onSubmit={formik.handleSubmit}>
+            <FormControl
+              isInvalid={formik.errors.email && formik.touched.email}
+            >
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                id="email"
+                name="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+              {!formik.errors.email && (
+                <FormHelperText>We'll never share your email.</FormHelperText>
+              )}
+              {formik.errors.email && (
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              )}
+            </FormControl>
 
-          <FormControl
-            isInvalid={formik.errors.password && formik.touched.password}
-            marginTop={8}
-          >
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-            />
-            {formik.errors.password && (
-              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-            )}
-          </FormControl>
-          <Button mt={4} colorScheme="teal" type="submit">
-            Login
-          </Button>
-        </form>
-      </Box>
+            <FormControl
+              isInvalid={formik.errors.password && formik.touched.password}
+              marginTop={8}
+            >
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+              {formik.errors.password && (
+                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+              )}
+            </FormControl>
+            <Button mt={4} colorScheme="teal" type="submit">
+              Login
+            </Button>
+          </form>
+        </Box>
+      </Center>
     </ChakraProvider>
   );
 }
