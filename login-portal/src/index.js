a721be77121232ccae475783200494f213f9c19f
lws803 <lws803@gmail.com>
2022-02-05 02:14:09 +0800

    creating login form

--- login-portal/src/index.js
+++ login-portal/src/index.js
@@ -1,13 +1,75 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  Box,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  FormErrorMessage,
+  Button,
+} from '@chakra-ui/react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import './style';
 
 export default function App() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    }),
+    validateOnChange: false,
+  });
+
   return (
     <ChakraProvider>
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
+      <Box p={8} maxWidth={500}>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl isInvalid={formik.errors.email && formik.touched.email}>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            {!formik.errors.email && (
+              <FormHelperText>We'll never share your email.</FormHelperText>
+            )}
+            {formik.errors.email && (
+              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+            )}
+          </FormControl>
+
+          <FormControl
+            isInvalid={formik.errors.password && formik.touched.password}
+            marginTop={8}
+          >
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+            {formik.errors.password && (
+              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+            )}
+          </FormControl>
+          <Button mt={4} colorScheme="teal" type="submit">
+            Login
+          </Button>
+        </form>
+      </Box>
     </ChakraProvider>
   );
 }
