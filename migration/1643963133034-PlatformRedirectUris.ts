f5e6373a3f3df74a7f914627f774246fe8a2db3e
lws803 <lws803@gmail.com>
2022-02-04 16:45:52 +0800

    prettified migration

--- migration/1643963133034-PlatformRedirectUris.ts
+++ migration/1643963133034-PlatformRedirectUris.ts
@@ -1,14 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class PlatformRedirectUris1643963133034 implements MigrationInterface {
-    name = 'PlatformRedirectUris1643963133034'
+  name = 'PlatformRedirectUris1643963133034';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`platforms\` ADD \`redirect_uris\` json NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`platforms\` ADD \`redirect_uris\` json NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`platforms\` DROP COLUMN \`redirect_uris\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`platforms\` DROP COLUMN \`redirect_uris\``,
+    );
+  }
 }
