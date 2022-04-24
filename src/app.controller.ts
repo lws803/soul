2bbd30e7692c73699ae8a137900af0a488bc7c10
lws803 <lws803@gmail.com>
2022-04-24 15:12:58 +0100

    exclude healthcheck endpoint from api docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 import { LocalIpWhitelistInterceptor } from './common/interceptors/local-ip-whitelist.interceptor';
@@ -8,6 +9,7 @@ import { LocalIpWhitelistInterceptor } from './common/interceptors/local-ip-whit
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiExcludeEndpoint()
   @Get('healthcheck')
   async getHealthcheck(): Promise<{ status: 'OK' }> {
     return await this.appService.getHealthcheck();
