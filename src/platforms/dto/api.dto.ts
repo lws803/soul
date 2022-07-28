3511ff11ab07994cda2b133482da2359239916f1
lws803 <lws803@gmail.com>
2022-07-29 00:20:56 +0800

    fix

--- src/platforms/dto/api.dto.ts
+++ src/platforms/dto/api.dto.ts
@@ -1,5 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { Expose, Transform, Type } from 'class-transformer';
 import {
   ArrayMaxSize,
