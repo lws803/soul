40e9285fbb2c24dae75f72b4ad748213081aae08
lws803 <lws803@gmail.com>
2022-08-04 00:13:23 +0800

    removed unwanted imports

--- src/common/interceptors/request-logging.interceptor.ts
+++ src/common/interceptors/request-logging.interceptor.ts
@@ -5,13 +5,12 @@ import {
   CallHandler,
   Logger,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class RequestLoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger(RequestLoggingInterceptor.name);
 
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler) {
     const httpContext = context.switchToHttp();
     const request = httpContext.getRequest();
     this.logger.log(`${request.method} ${request.url}`);
