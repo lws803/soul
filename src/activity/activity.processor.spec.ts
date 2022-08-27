13a622a2c80be8e2d774be6e8917f6f9f4724d9b
lws803 <lws803@gmail.com>
2022-08-28 02:05:38 +0800

    added test

---
+++ src/activity/activity.processor.spec.ts
@@ -0,0 +1,73 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Job } from 'bull';
+import axios from 'axios';
+
+import * as factories from 'factories';
+import { PlatformUser } from 'src/platforms/entities/platform-user.entity';
+
+import { ActivityProcessor } from './activity.processor';
+import { FollowActivityJobPayload } from './types';
+
+describe(ActivityProcessor, () => {
+  let processor: ActivityProcessor;
+  const fromUser = factories.oneUser.build({
+    id: 1,
+    username: 'FROM_USER',
+    userHandle: 'FROM_USER#1',
+  });
+  const toUser = factories.oneUser.build({
+    id: 2,
+    username: 'TO_USER',
+    userHandle: 'TO_USER#2',
+  });
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ActivityProcessor,
+        {
+          provide: getRepositoryToken(PlatformUser),
+          useValue: {
+            findAndCount: jest
+              .fn()
+              .mockResolvedValue([
+                [factories.onePlatformUser.build({ user: toUser })],
+                1,
+              ]),
+          },
+        },
+      ],
+    }).compile();
+
+    module.useLogger(false);
+
+    processor = module.get<ActivityProcessor>(ActivityProcessor);
+    jest.spyOn(axios, 'post');
+  });
+
+  describe('processActivity()', () => {
+    it('sends follow activity successfully', async () => {
+      const response = await processor.processActivity({
+        data: { fromUser, toUser, type: 'FOLLOW' },
+      } as Job<FollowActivityJobPayload>);
+
+      expect(response).toBeUndefined();
+
+      const postAction = jest.spyOn(axios, 'post');
+      expect(postAction).toHaveBeenCalledWith('ACTIVITY_WEBHOOK_URI', {
+        fromUser: {
+          id: fromUser.id,
+          username: fromUser.username,
+          userHandle: fromUser.userHandle,
+        },
+        toUser: {
+          id: toUser.id,
+          username: toUser.username,
+          userHandle: toUser.userHandle,
+        },
+        type: 'FOLLOW',
+      });
+    });
+  });
+});
