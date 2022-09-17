024ec5f2c5287c3547ccd1c9a1099547fb34b3c2
lws803 <lws803@gmail.com>
2022-09-17 13:38:25 +0800

    added descriptions for api responses for platforms

--- src/platforms/serializers/api-responses.entity.ts
+++ src/platforms/serializers/api-responses.entity.ts
@@ -6,19 +6,27 @@ import { UserRole } from 'src/roles/role.enum';
 import { FindOneUserResponseEntity } from 'src/users/serializers/api-responses.entity';
 
 export class PlatformCategoryResponseEntity {
-  @ApiResponseProperty({ name: 'id', example: 1 })
+  @ApiResponseProperty({
+    name: 'id',
+    example: 1,
+    description: 'ID of a platform category.',
+  })
   id: number;
 
   @ApiResponseProperty({
     name: 'name',
-    description: 'One of predefined category names for the platform.',
-    example: 'tech',
+    description: 'A predefined category name for platforms.',
+    example: 'journalism',
   })
   name: string;
 }
 
 export class FullPlatformResponseEntity extends CreatedAtUpdatedAtEntity {
-  @ApiResponseProperty({ name: 'id', example: 1 })
+  @ApiResponseProperty({
+    name: 'id',
+    example: 1,
+    description: 'ID of the platform.',
+  })
   id: number;
 
   @ApiResponseProperty({
@@ -31,14 +39,17 @@ export class FullPlatformResponseEntity extends CreatedAtUpdatedAtEntity {
   @ApiResponseProperty({
     name: 'name_handle',
     example: 'soul#1',
-    description: 'Includes the id number after the # symbol',
+    description:
+      'Name handle is constructed using the name and ID of the platform. ' +
+      'ID is present after the # symbol.',
   })
   nameHandle: string;
 
   @ApiResponseProperty({
     name: 'is_verified',
     example: true,
-    description: 'Is the platform verified, used only for official platforms',
+    description:
+      'Used to identify verified platforms, used only for official platforms',
   })
   isVerified: boolean;
 
@@ -61,6 +72,7 @@ export class FullPlatformResponseEntity extends CreatedAtUpdatedAtEntity {
   @ApiResponseProperty({
     name: 'category',
     type: PlatformCategoryResponseEntity,
+    description: 'Categories associated with this platform.',
   })
   @Type(() => PlatformCategoryResponseEntity)
   category?: PlatformCategoryResponseEntity;
@@ -71,7 +83,11 @@ export class CreatePlatformResponseEntity extends FullPlatformResponseEntity {}
 export class UpdatePlatformResponseEntity extends FullPlatformResponseEntity {}
 
 export class FindOnePlatformResponseEntity extends CreatedAtUpdatedAtEntity {
-  @ApiResponseProperty({ name: 'id', example: 1 })
+  @ApiResponseProperty({
+    name: 'id',
+    example: 1,
+    description: 'ID of the platform.',
+  })
   id: number;
 
   @ApiResponseProperty({
@@ -84,20 +100,24 @@ export class FindOnePlatformResponseEntity extends CreatedAtUpdatedAtEntity {
   @ApiResponseProperty({
     name: 'name_handle',
     example: 'soul#1',
-    description: 'Includes the id number after the # symbol',
+    description:
+      'Name handle is constructed using the name and ID of the platform. ' +
+      'ID is present after the # symbol.',
   })
   nameHandle: string;
 
   @ApiResponseProperty({
     name: 'is_verified',
     example: true,
-    description: 'Is the platform verified, used only for official platforms',
+    description:
+      'Used to identify verified platforms, used only for official platforms',
   })
   isVerified: boolean;
 
   @ApiResponseProperty({
     name: 'category',
     type: PlatformCategoryResponseEntity,
+    description: 'Categories associated with this platform.',
   })
   @Type(() => PlatformCategoryResponseEntity)
   category?: PlatformCategoryResponseEntity;
@@ -111,12 +131,21 @@ export class FindAllPlatformResponseEntity {
   @Type(() => FindOnePlatformResponseEntity)
   platforms: FindOnePlatformResponseEntity[];
 
-  @ApiResponseProperty({ name: 'total_count', example: 100 })
+  @ApiResponseProperty({
+    name: 'total_count',
+    example: 10,
+    description:
+      'Total count is used to determine the total number of platforms irregardless of pagination.',
+  })
   totalCount: number;
 }
 
 class FindOnePlatformUserResponseEntity {
-  @ApiResponseProperty({ name: 'id', example: 1 })
+  @ApiResponseProperty({
+    name: 'id',
+    example: 1,
+    description: 'ID of a platform user.',
+  })
   id: number;
 
   @ApiResponseProperty({ name: 'user', type: FindOneUserResponseEntity })
@@ -130,7 +159,13 @@ class FindOnePlatformUserResponseEntity {
   @Type(() => FindOnePlatformResponseEntity)
   platform: FindOnePlatformResponseEntity;
 
-  @ApiResponseProperty({ name: 'roles', example: [UserRole.Admin] })
+  @ApiResponseProperty({
+    name: 'roles',
+    example: [UserRole.Admin, UserRole.Member],
+    description: 'User roles for a platform.',
+    enum: Object.values(UserRole),
+    type: [UserRole],
+  })
   roles: UserRole[];
 }
 
@@ -142,7 +177,12 @@ export class FindAllPlatformUsersResponseEntity {
   @Type(() => FindOnePlatformUserResponseEntity)
   platformUsers: FindOnePlatformUserResponseEntity[];
 
-  @ApiResponseProperty({ name: 'total_count', example: 100 })
+  @ApiResponseProperty({
+    name: 'total_count',
+    example: 100,
+    description:
+      'Total count is used to determine the total number of platform users irregardless of pagination.',
+  })
   totalCount: number;
 }
 
