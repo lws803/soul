63cc3c8fb53f23534ce59c7caaf4aa227fd3c02a
lws803 <lws803@gmail.com>
2022-09-18 15:07:29 +0800

    remove local whitelist ip interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
-import { LocalIpWhitelistInterceptor } from './common/interceptors/local-ip-whitelist.interceptor';
 
 @Controller()
-@UseInterceptors(LocalIpWhitelistInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // TODO: Maybe re-expose this endpoint again
   @ApiExcludeEndpoint()
   @Get('healthcheck')
   async healthcheck(): Promise<{ status: 'OK' }> {
