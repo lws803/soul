75a46db5d1d6b66161806dcf21eb721ec3edad88
lws803 <lws803@gmail.com>
2022-09-27 16:53:33 +0800

    added cleanup task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -27,6 +27,18 @@ export class TasksService {
       })
       .orWhere('refresh_tokens.is_revoked = :isRevoked', { isRevoked: true })
       .execute();
+
+    // Deletes all refresh tokens for platform users with count above 10
+    await this.refreshTokenRepository.manager.query(
+      `DELETE tokens FROM refresh_tokens tokens JOIN
+        (
+          SELECT user_id, platform_user_id, COUNT(*) as cnt
+            FROM refresh_tokens GROUP BY user_id, platform_user_id HAVING cnt > 10
+        )
+        tmp ON tmp.platform_user_id = tokens.platform_user_id;
+      `,
+    );
+
     this.logger.debug('Deleted all expired refresh tokens');
     this.logger.log({
       refreshTokensCountAfterDelete: await this.refreshTokenRepository.count(),
---
+++ test-e2e/tasks.e2e-spec.ts
@@ -0,0 +1,168 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { Repository, Connection } from 'typeorm';
+import * as sha256 from 'crypto-js/sha256';
+import base64url from 'base64url';
+
+import { User } from 'src/users/entities/user.entity';
+import { RefreshToken } from 'src/auth/entities/refresh-token.entity';
+import { PlatformUser } from 'src/platforms/entities/platform-user.entity';
+import { Platform } from 'src/platforms/entities/platform.entity';
+import { PlatformCategory } from 'src/platforms/entities/platform-category.entity';
+import { TasksService } from 'src/tasks/tasks.service';
+
+import * as factories from '../factories';
+
+import createAppFixture from './fixtures/create-app-fixture';
+import { createUsersAndLoginFixture } from './fixtures/create-users-and-login-fixture';
+
+describe('TasksModule (e2e)', () => {
+  let app: INestApplication;
+  let userRepository: Repository<User>;
+  let refreshTokenRepository: Repository<RefreshToken>;
+  let platformUserRepository: Repository<PlatformUser>;
+  let platformRepository: Repository<Platform>;
+  let platformCategoryRepository: Repository<PlatformCategory>;
+  let tasksService: TasksService;
+
+  const codeVerifier = 'CODE_VERIFIER';
+  const codeChallenge = base64url(sha256(codeVerifier).toString(), 'hex');
+
+  beforeAll(async () => {
+    app = await createAppFixture({});
+    await app.init();
+    app.useLogger(false);
+
+    const connection = app.get(Connection);
+    await connection.synchronize(true);
+
+    tasksService = app.get<TasksService>(TasksService);
+
+    userRepository = connection.getRepository(User);
+    refreshTokenRepository = connection.getRepository(RefreshToken);
+    platformUserRepository = connection.getRepository(PlatformUser);
+    platformRepository = connection.getRepository(Platform);
+    platformCategoryRepository = connection.getRepository(PlatformCategory);
+
+    await platformCategoryRepository.save(
+      factories.platformCategoryEntity.build(),
+    );
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('cleanupExpiredRefreshTokens', () => {
+    let userAccount: {
+      accessToken: string;
+      refreshToken: string;
+      user: User;
+    };
+
+    beforeAll(async () => {
+      const [firstUser] = await createUsersAndLoginFixture(app);
+      userAccount = firstUser;
+
+      const platform = await platformRepository.save(
+        factories.platformEntity.build({
+          redirectUris: ['https://www.example.com'],
+        }),
+      );
+      await platformUserRepository.save(
+        factories.platformUserEntity.build({
+          platform,
+          user: userAccount.user,
+        }),
+      );
+    });
+
+    afterAll(async () => {
+      await refreshTokenRepository.delete({});
+      await platformUserRepository.delete({});
+      await platformRepository.delete({});
+      await userRepository.delete({});
+    });
+
+    it('clears tokens for platforms exceeding 10', async () => {
+      await request(app.getHttpServer())
+        .post('/auth/refresh')
+        .send({ refresh_token: userAccount.refreshToken })
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          expect(res.headers['cache-control']).toBe('no-store');
+          expect(res.body).toEqual({
+            access_token: expect.any(String),
+            refresh_token: expect.any(String),
+            expires_in: 900,
+          });
+        });
+
+      const platformId = 1;
+      for (let i = 0; i < 12; i++) {
+        const params = new URLSearchParams({
+          code_challenge: codeChallenge,
+          state: 'TEST_STATE',
+          redirect_uri: 'https://www.example.com',
+          client_id: String(platformId),
+        });
+        const codeResp = await request(app.getHttpServer())
+          .post(`/auth/code?${params.toString()}`)
+          .send({ email: 'TEST_USER@EMAIL.COM', password: '1oNc0iY3oml5d&%9' });
+        const resp = await request(app.getHttpServer())
+          .post('/auth/verify')
+          .send({
+            code: codeResp.body.code,
+            redirect_uri: 'https://www.example.com',
+            code_verifier: codeVerifier,
+          });
+
+        const { refresh_token } = resp.body;
+        const refreshResp = await request(app.getHttpServer())
+          .post('/auth/refresh')
+          .send({ refresh_token, client_id: 1 });
+        expect(refreshResp.status).toBe(HttpStatus.OK);
+      }
+
+      const platformUser = await platformUserRepository
+        .createQueryBuilder('platform_user')
+        .where('user_id = :userId', {
+          userId: userAccount.user.id,
+        })
+        .andWhere('platform_id = :platformId', { platformId })
+        .getOne();
+
+      expect(
+        await refreshTokenRepository.count({ platformUser: platformUser }),
+      ).toEqual(12);
+
+      expect(
+        await platformUserRepository
+          .createQueryBuilder('platform_user')
+          .where('user_id = :userId', {
+            userId: userAccount.user.id,
+          })
+          .andWhere('platform_id IS NULL')
+          .getOne(),
+      ).not.toBeNull();
+
+      await tasksService.cleanupExpiredRefreshTokens();
+
+      // Other platform refresh tokens should not be affected
+      expect(
+        await platformUserRepository
+          .createQueryBuilder('platform_user')
+          .where('user_id = :userId', {
+            userId: userAccount.user.id,
+          })
+          .andWhere('platform_id IS NULL')
+          .getOne(),
+      ).not.toBeNull();
+
+      // Only the one with count above 10 should be deleted
+      expect(
+        await refreshTokenRepository.count({ platformUser: platformUser }),
+      ).toEqual(0);
+    });
+  });
+});
