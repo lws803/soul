a039aae6c5c9dadef532baaa06137aee62914455
lws803 <lws803@gmail.com>
2022-10-30 15:30:53 +0800

    migrate user entity to prisma

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { MailModule } from 'src/mail/mail.module';
 import { RefreshToken } from 'src/auth/entities/refresh-token.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
@@ -11,7 +12,7 @@ import { User } from './entities/user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([User, RefreshToken]), MailModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
   exports: [UsersService],
 })
 export class UsersModule {}
