2f159c37c1e396faedffe7e676d868737057d331
lws803 <lws803@gmail.com>
2022-11-05 01:48:09 +0800

    removed unused error

--- src/platforms/exceptions/index.ts
+++ src/platforms/exceptions/index.ts
@@ -2,6 +2,5 @@ export * from './no-admins-remaining.exception';
 export * from './platform-not-found.exception';
 export * from './platform-user-not-found.exception';
 export * from './duplicate-platform-user.exception';
-export * from './duplicate-platform.exception';
 export * from './platform-category-not-found.exception';
 export * from './max-admin-roles-per-user.exception';
